f2260710dde5c07387e5fece2cc21b52371bcccc
Jordy Lima <jordy.sousa@kamaleon.com.br>
2022-05-01 21:37:40 -0300

    Primeiro Componente #01

---
+++ exercicios/exercicios/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+function App() { //retorna por jsx - sempre precisa do modulo React pra retornar jsx
+    return <Text>Primeiro Componente</Text>
+}
+
+export default App
