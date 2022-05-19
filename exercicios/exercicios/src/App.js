0db0b531d779660e97b71cd97c640d667812f827
jordylimaeng <jordylimaeng@github.com>
2022-05-19 00:01:37 -0300

    Using props.children

--- exercicios/exercicios/src/App.js
+++ exercicios/exercicios/src/App.js
@@ -1,37 +1,23 @@
 import React from 'react'
 import { StyleSheet, SafeAreaView } from 'react-native'
 
-// import C1 from './components/Primeiro'
-// import CompOriginal, { Comp1, Comp2 } from './components/Multi'
-// import MinMax from './components/MinMax'
-// import Titulo from './components/Titulo'
-// import Botao from './components/Botao'
-// import Contador from './components/Contador'
-// import Pai from './components/indireta/Pai'
-// import ContadorV2 from './components/contador/ContadorV2'
-import Diferenciar from './components/Diferenciar'
-import Parimpar from './components/Parimpar'
+import Familia from './components/relacao/Familia'
+import Membro from './components/relacao/Membro'
 
 export default () => (
     //retorna por jsx - sempre precisa do modulo React pra retornar jsx
     //também podemos exportar como arrow function
     <SafeAreaView style={style.App}>
-        {/* <C1/>
-        <Comp1/>
-        <Comp2/>
-        <CompOriginal/> */}
-        {/* <MinMax n1={3} n2={20}/>
-        <MinMax n1={15} n2={30}/> 
-        <Titulo principal="Cadastro"
-                secundario="Tela de Cadastro do Produto"
-        <Botao/> 
-        <Contador initial={100} step={30}/>
-        <Contador />
-        <Pai/>
-        <ContadorV2/>
-        />*/}
-        <Diferenciar/>
-        <Parimpar num={2}/>
+        <Familia>
+            <Membro nome='Bia' sobrenome='Arruda'/>
+            <Membro nome='Bernardo' sobrenome='Arruda'/>
+        </Familia>
+        <Familia>
+            <Membro nome='Ana' sobrenome='Silva'/>
+            <Membro nome='Arnaldo' sobrenome='Silva'/>
+            <Membro nome='Gui' sobrenome='Silva'/>            
+            <Membro nome='Rebeca' sobrenome='Silva'/>
+        </Familia>
    </SafeAreaView>
 
 )
