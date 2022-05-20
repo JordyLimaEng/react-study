469fb15acaae52bfb604b2d299787036294fbaaf
jordylimaeng <jordylimaeng@github.com>
2022-05-19 21:53:26 -0300

    Watch out with .This

--- exercicios/exercicios/src/components/mega/Mega.js
+++ exercicios/exercicios/src/components/mega/Mega.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react'
-import { Text } from 'react-native'
+import { Text, TextInput } from 'react-native'
 import style from '../style'
 
 export default class Mega extends Component {
-    
+
+    state = {
+        qtdNumeros: this.props.qtdNumeros
+    }
+
+    alterarQtdeNumero(qtd) {
+        this.setState({ qtdNumeros: qtd })
+    }
+
     render() {
         return (
-            <Text style={style.txtG}>
-                Gerador de Mega Sena {this.props.qtdNumeros}
-            </Text>
+            <>
+                <Text style={style.txtG}>
+                    Gerador de Mega Sena {this.state.qtdNumeros}
+                </Text>
+                <TextInput
+                    keyboardType={'numeric'}
+                    style={{ borderBottomWidth: 1 }}
+                    placeholder='Qtde de numeros'
+                    value={this.state.qtdNumeros}
+                    onChangeText={qtd => this.alterarQtdeNumero(qtd)}
+                />
+            </>
         )
     }
 }
