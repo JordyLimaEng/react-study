275d85887b3fa648e933cdf9d5090ad6141561c1
jordylimaeng <jordylimaeng@github.com>
2022-05-23 00:46:03 -0300

    Simple test of navigation between screens

--- react_navigation/src/views/ScreenA.js
+++ react_navigation/src/views/ScreenA.js
@@ -2,9 +2,10 @@ import React from 'react'
 import CentralText from '../components/CentralText'
 
 export default props => {
-   return(
-      <CentralText backColor='#e53935'>
-          Screen A
-      </CentralText>
-   )
+    props.navigation.navigate('ScreenB')
+    return (
+        <CentralText backColor='#e53935'>
+            Screen A
+        </CentralText>
+    )
 }
