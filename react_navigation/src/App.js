9b4e7986ea1940f41c520b8af5bd7954cf0913df
jordylimaeng <jordylimaeng@github.com>
2022-05-23 00:29:23 -0300

    Create Screens to implements navigation

--- react_navigation/src/App.js
+++ react_navigation/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import { SafeAreaView, Text } from 'react-native'
 import CentralText from './components/CentralText'
+import ScreenA from './views/ScreenA'
+import ScreenB from './views/ScreenB'
+import ScreenC from './views/ScreenC'
 
 export default props => (
     <SafeAreaView style={{ flex: 1 }}>
-        <CentralText> App </CentralText>
+        <ScreenA />
+        <ScreenB />
+        <ScreenC />
     </SafeAreaView>
+    
 )
---
+++ react_navigation/src/views/ScreenA.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import CentralText from '../components/CentralText'
+
+export default props => {
+   return(
+      <CentralText backColor='#e53935'>
+          Screen A
+      </CentralText>
+   )
+}
